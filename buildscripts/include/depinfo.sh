d97d437a7430db04ec8be16df21c53c504c9eab6
hatharry <hatharry@hotmail.com>
2018-10-30 13:34:33 +1300

    Update ffmpeg

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -28,7 +28,7 @@ dep_mpv_android=(mpv)
 ## Travis-related
 
 # pinned ffmpeg commit used by travis-ci
-v_travis_ffmpeg=be0b77e6e83b61c2da338201b5ddfae1c9acedc5
+v_travis_ffmpeg=c0692cb2bb3b51660b501f14bd344005c68df465
 
 # filename used to uniquely identify a build prefix
 travis_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-libass-${v_libass}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-ffmpeg-${v_travis_ffmpeg}.tgz"
